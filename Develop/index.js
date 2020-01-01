a7b6b749ce0e170345c0edab24332cf789646f99
Alex Dollard <alexdollard141@gmail.com>
2020-01-01 17:20:39 -0600

    moved 2 main files over from homework folder

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const electron = require("electron");
+const electron_html_to = require("electron-html-to");
+
+
+
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
