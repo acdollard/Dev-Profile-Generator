6fcd13ec0ee7340164533bbdfa8dd2faa4f57c34
Alex Dollard <alexdollard141@gmail.com>
2020-01-01 17:53:36 -0600

    got inquirer and axios call working

--- Develop/index.js
+++ Develop/index.js
@@ -1,20 +1,40 @@
-const fs = require("fs");
+// const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const electron = require("electron");
-const electron_html_to = require("electron-html-to");
+// const electron = require("electron");
+// const electron_html_to = require("electron-html-to");
 
 
 
+inquirer
+  .prompt(
+    {
+    message: "Enter your GitHub username:",
+    name: "username"
+    },
+    {
+    message: "What is your favorite color?",
+    name: "favoriteColor"
+    }
+    ).then(function({ username }){
+    const queryUrl = `https://api.github.com/users/${username}`
+    axios
+        .get(queryUrl)
+        .then(function(res){
+            console.log(res)
+        })
+    })
 
-const questions = [
+
+
+// const questions = [
   
-];
+// ];
 
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
  
-}
+// }
 
-function init() {
+// function init() {
 
-init();
+// init();
